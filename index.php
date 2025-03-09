d3027ca31f55e1ce2241f064ca0d86b9a4697f49
Jose Molina <israel.molina@gmail.com>
2025-03-09 01:27:30 -0500

    Initial full structure (#1)
    * Add Docker configuration file
    
    * Rename index to PHP
    
    * Rename index to PHP
    
    * Add SASS to Docker compose file and CSS logic.
    
    * Initial CSS styles
    
    * Initial CSS styles
    
    * Clean files

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Rookiegaminglab.com</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" >
+    <meta name="description" content="Rookie Gaming Lab - Your go-to source for gaming tutorials, reviews, and news. Follow us on YouTube, TikTok, Facebook, and Instagram for the latest updates.">
+    <meta name="keywords" content="gaming, tutorials, reviews, news, YouTube, TikTok, Facebook, Instagram">
+    <meta name="author" content="Jose Molina">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/public/assets/img/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/general.css">
+  </head>
+
+  <body>
+    <div class="container" id="error" style="padding: 20px">
+
+      <div class="msn-wrapper mx-auto text-center">
+        <img src="/public/assets/img/rookiegaminglab-logo-full.jpg" alt="Rookie Gaming Lab Logo" class="logo img-fluid w-100">
+        <h1 class="bungee-regular d-flex flex-column justify-content-center lh-1 my-3">
+          <span>Rookie</span>
+          <span class="primary-color">Gaming Lab</span>
+        </h1>
+        <hr class="w-50 mx-auto">  
+        <p class="lead">
+          Puedes encontrarme en:
+        </p>
+        <div class="d-flex flex-sm-row flex-column justify-content-center gap-2">
+          <a href="https://www.youtube.com/@rookiegaminglab" target="_blank" class="btn btn-outline-primary btn-lg">
+            <i class="bi bi-youtube display-4 position-relative"></i>
+          </a>
+          <a href="https://www.tiktok.com/@rookiegaminglab" target="_blank" class="btn btn-outline-primary btn-lg">
+            <i class="bi bi-tiktok display-4"></i>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=61573574177710" target="_blank" class="btn btn-outline-primary btn-lg">
+            <i class="bi bi-facebook display-4"></i>
+          </a>
+          <a href="https://www.instagram.com/rookiegaminglab" target="_blank" class="btn btn-outline-primary btn-lg">
+            <i class="bi bi-instagram display-4"></i>
+          </a>
+        </div>
+      </div>
+
+    </div>
+  </body>
+</html>
