1178ce43e69e71b70a5e3fed862c2d4c6204158a
Jose Molina <israel.molina@gmail.com>
2025-03-09 01:10:41 -0500

    Initial CSS styles

--- index.php
+++ index.php
@@ -1,87 +1,51 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Rookiegaminglab.com</title>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" >
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style type="text/css">
-body {
-  /*background: linear-gradient(90deg, white, gray);*/
-  background-color: #eee;
-}
+  <head>
+    <title>Rookiegaminglab.com</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" >
+    <meta name="description" content="Rookie Gaming Lab - Your go-to source for gaming tutorials, reviews, and news. Follow us on YouTube, TikTok, Facebook, and Instagram for the latest updates.">
+    <meta name="keywords" content="gaming, tutorials, reviews, news, YouTube, TikTok, Facebook, Instagram">
+    <meta name="author" content="Jose Molina">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/public/assets/img/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" type="text/css" href="/public/assets/css/general.css">
+  </head>
 
-body, h1, p {
-  background-color: white;
-  font-family: "Helvetica Neue", "Segoe UI", Segoe, Helvetica, Arial, "Lucida Grande", sans-serif;
-  font-weight: normal;
-  margin: 0;
-  padding: 0;
-  text-align: center;
-}
+  <body>
+    <div class="container" id="error" style="padding: 20px">
 
-@media only screen and (max-width: 600px) {
-  body {
-    font-size: 1.3em !important;
-  }
-}
+      <div class="msn-wrapper mx-auto text-center">
+        <img src="/public/assets/img/rookiegaminglab-logo-full.jpg" alt="Rookie Gaming Lab Logo" class="logo img-fluid w-100">
+        <h1 class="bungee-regular d-flex flex-column justify-content-center lh-1 my-3">
+          <span>Rookie</span>
+          <span class="primary-color">Gaming Lab</span>
+        </h1>
+        <hr class="w-50 mx-auto">  
+        <p class="lead">
+          Puedes encontrarme en:
+        </p>
+        <div class="d-flex flex-sm-row flex-column justify-content-center gap-2">
+          <a href="https://www.youtube.com/@rookiegaminglab" target="_blank" class="btn btn-outline-primary btn-lg">
+            <i class="bi bi-youtube display-4 position-relative"></i>
+          </a>
+          <a href="https://www.tiktok.com/@rookiegaminglab" target="_blank" class="btn btn-outline-primary btn-lg">
+            <i class="bi bi-tiktok display-4"></i>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=61573574177710" target="_blank" class="btn btn-outline-primary btn-lg">
+            <i class="bi bi-facebook display-4"></i>
+          </a>
+          <a href="https://www.instagram.com/rookiegaminglab" target="_blank" class="btn btn-outline-primary btn-lg">
+            <i class="bi bi-instagram display-4"></i>
+          </a>
+        </div>
+      </div>
 
-.container {
-  margin-left:  auto;
-  margin-right:  auto;
-  margin-top: 177px;
-  max-width: 1170px;
-  padding-right: 15px;
-  padding-left: 15px;
-}
-
-.row:before, .row:after {
-  display: table;
-  content: " ";
-}
-
-h1 {
-  font-size: 48px;
-  font-weight: 300;
-  margin: 0 0 20px 0;
-}
-
-@media only screen and (max-width: 600px) {
-  h1 {
-    font-size: 25px;
-  }
-}
-
-.lead {
-  font-size: 21px;
-  font-weight: 200;
-  margin-bottom: 20px;
-}
-
-p {
-  margin: 0 0 10px;
-}
-
-a {
-  color: #3282e6;
-  text-decoration: none;
-}
-</style>
-</head>
-
-<body>
-<div class="container text-center" id="error" style="padding: 20px">
-
-  <div class="row">
-    <div class="col-md-12">
-      <h1><b>Rookiegaminglab.com</b></h1>
-      <p class="lead"><a href="mailto:rookie@rookiegaminglab.com">rookie@rookiegaminglab.com</a></p>
-      <p>
-        Comming Soon!
-      </p>
     </div>
-  </div>
-
-</div>
-
-</body>
+  </body>
 </html>
